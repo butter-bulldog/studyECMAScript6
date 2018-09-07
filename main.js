2d2ad2c3f887ddeef5c2d10e8b43e4b46be319d6
FumiakiSaito <haratairani3000points@gmail.com>
2018-09-07 23:32:39 +0900

    import文のバリエーションを追加

--- main.js
+++ main.js
@@ -2,3 +2,8 @@
 import {triangle, circle} from './figure';
 console.log(triangle(10, 5));
 console.log(circle(2));
+
+// 全関数をimportし、特定の名前からアクセスできるようにする方法
+import * as mymath from './figure';
+console.log(mymath.triangle(10, 5));
+console.log(mymath.circle(2));
